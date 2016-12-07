77f9fba2a9e0d78d5e4ce73081f9c34e086bd9f5
oleglavr <oleglavr@ukr.net>
2016-12-07 23:54:13 +0200

    add ingredients add

---
+++ src/main/java/lab/epam/olavr/servlets/AddIngredients.java
@@ -0,0 +1,44 @@
+package lab.epam.olavr.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Servlet implementation class AddIngredients
+ */
+public class AddIngredients extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final Logger log = Logger.getLogger(AddIngredients.class);  
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddIngredients() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if (request.getParameter("Coffee")!=null){
+		
+		}
+		  request.getRequestDispatcher("/pages/administration.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
