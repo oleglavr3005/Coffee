9818234169db9f591ddd23522f9d0f1b4c41dc7f
oleglavr <oleglavr@ukr.net>
2016-12-20 23:54:07 +0200

    fix email

---
+++ src/main/java/lab/epam/olavr/service/MailService.java
@@ -0,0 +1,44 @@
+package lab.epam.olavr.service;
+
+import javax.mail.*;
+import javax.mail.event.MailEvent;
+import javax.mail.internet.*;
+
+import java.util.Properties;
+
+import javax.activation.*;
+
+public class MailService {
+	
+	static Properties mailServerProperties;
+	static Session getMailSession;
+	static MimeMessage generateMailMessage;
+ 
+	public static void generateAndSendEmail(String email,String subject,String body) throws AddressException, MessagingException {
+ 
+		// Step1
+		System.out.println("\n 1st ===> setup Mail Server Properties..");
+		mailServerProperties = System.getProperties();
+		mailServerProperties.put("mail.smtp.port", "587");
+		mailServerProperties.put("mail.smtp.auth", "true");
+		mailServerProperties.put("mail.smtp.starttls.enable", "true");
+ 
+		getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+		generateMailMessage = new MimeMessage(getMailSession);
+		generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+		
+		generateMailMessage.setSubject(subject);
+		String emailBody =body;
+		generateMailMessage.setContent(emailBody, "text/html");
+		System.out.println("Mail Session has been created successfully...");
+		generateMailMessage.setFrom("oleglavr3005@gmail.com");
+		
+		Transport transport = getMailSession.getTransport("smtp");
+ 
+		// Enter your correct gmail UserID and Password
+		// if you have 2FA enabled then provide App Specific Password
+		transport.connect("smtp.gmail.com", "oleglavr3005", "pifagorus");
+		transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+		transport.close();
+	}
+}
